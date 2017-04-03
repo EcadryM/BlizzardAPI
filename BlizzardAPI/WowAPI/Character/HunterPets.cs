434c1eb57fc4970cbb2dc56041a6fff8163724a1
Serhii Hrydzhuk <stab1e@outlook.com>
2017-04-03 21:07:27 +0300

    Created character hunter pets class
    Created character hunter pets class with supporting classes:
    "HunterPet.cs" and "Spec.cs". Each of new classes has its own
    properties.

---
+++ BlizzardAPI/WowAPI/Character/HunterPets.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace WowAPI.Character
+{
+    class HunterPets
+    {
+        public List<HunterPet> hunterPets = new List<HunterPet>();
+
+        public HunterPets(string name, string realm, string region, string locale)
+        {
+            // TODO: Get the JSON file from the URL of the api request and initialize the properties with json objects values.
+        }
+    }
+}

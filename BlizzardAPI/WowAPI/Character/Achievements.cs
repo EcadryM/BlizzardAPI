02a90763a099bf078a49d7629fde55b1f068911a
Serhii Hrydzhuk <stab1e@outlook.com>
2017-04-03 20:24:22 +0300

    Created character achievements class
    Created character achievements class with set of properties

---
+++ BlizzardAPI/WowAPI/Character/Achievements.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WowAPI.Character
+{
+    class Achievements
+    {
+        public int[] AchievementsCompleted { get; set; }
+        public int[] Criteria { get; set; }
+
+        public long[] AchievementsCompletedTimestamp { get; set; }
+        public long[] CriteriaQuantity { get; set; }
+        public long[] CriteriaTimestamp { get; set; }
+        public long[] CriteriaCreated { get; set; }
+
+        public Achievements(string name, string realm, string region, string locale)
+        {
+            // TODO: Get the JSON file from the URL of the api request and initialize the properties with json objects values.
+        }
+    }
+}

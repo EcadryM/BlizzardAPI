87e95bf5aa81955ab6528db2b0c062f912050d5a
Serhii Hrydzhuk <stab1e@outlook.com>
2017-04-03 21:47:06 +0300

    Created "PvP", "Quest" and "Reputation" classes

---
+++ BlizzardAPI/WowAPI/Character/Quests.cs
@@ -0,0 +1,12 @@
+﻿namespace WowAPI.Character
+{
+    public class Quests
+    {
+        public int[] CharacterQuests { get; set; }
+
+        public Quests(string name, string realm, string region, string locale)
+        {
+            // TODO: Get the JSON file from the URL of the api request and initialize the properties with json objects values.
+        }
+    }
+}
---
+++ BlizzardAPI/WowAPI/Character/Reputation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace WowAPI.Character
+{
+    class Reputation
+    {
+        public List<ReputationInfo> reputation = new List<ReputationInfo>();
+
+        public Reputation(string name, string realm, string region, string locale)
+        {
+            // TODO: Get the JSON file from the URL of the api request and initialize the properties with json objects values.
+        }
+    }
+}

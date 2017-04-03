93ff7c853814bc2bf1e2e65a3bcea9eb3e8233d4
Serhii Hrydzhuk <stab1e@outlook.com>
2017-04-03 21:22:25 +0300

    Created character mounts and pets classes

---
+++ BlizzardAPI/WowAPI/Character/Mounts.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using WowAPI.Mounts;
+
+namespace WowAPI.Character
+{
+    class Mounts
+    {
+        public int NumCollected { get; set; }
+        public int NumNotCollected { get; set; }
+
+        public List<MountInfo> collected = new List<MountInfo>();
+
+        public Mounts(string name, string realm, string region, string locale)
+        {
+            // TODO: Get the JSON file from the URL of the api request and initialize the properties with json objects values.
+        }
+    }
+}
---
+++ BlizzardAPI/WowAPI/Character/Pets.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace WowAPI.Character
+{
+    class Pets
+    {
+        public int NumCollected { get; set; }
+        public int NumNotCollected { get; set; }
+
+        public List<Pet> collected = new List<Pet>();
+
+        public Pets(string name, string realm, string region, string locale)
+        {
+            // TODO: Get the JSON file from the URL of the api request and initialize the properties with json objects values.
+        }
+    }
+}

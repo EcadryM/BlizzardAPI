94926c67d977ca8a7e9249d809452a48d6199dfc
Serhii Hrydzhuk <stab1e@outlook.com>
2017-04-03 22:03:11 +0300

    Created character titles and title class

---
+++ BlizzardAPI/WowAPI/Character/Title.cs
@@ -0,0 +1,8 @@
+﻿namespace WowAPI.Character
+{
+    public class Title
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ BlizzardAPI/WowAPI/Character/Titles.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace WowAPI.Character
+{
+    public class Titles
+    {
+        public List<Title> titles = new List<Title>();
+
+        public Titles(string name, string realm, string region, string locale)
+        {
+            // TODO: Get the JSON file from the URL of the api request and initialize the properties with json objects values.
+        }
+    }
+}

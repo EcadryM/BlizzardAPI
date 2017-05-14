35e436936aded9a9d317c6a4c7752d03a367696d
Serhii Hrydzhuk <stab1e@outlook.com>
2017-05-14 12:17:59 +0300

    Created D3 project

---
+++ BlizzardAPI/BlizzardAPI.D3/ApiHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace BlizzardAPI.D3
+{
+    public class ApiHandler
+    {
+        public static string ApiKey { get; set; }
+
+        public ApiHandler(string apiKey)
+        {
+            ApiKey = apiKey;
+        }
+    }
+}
